2d979df7624a1d005891cfdfcc1466eccf3a0d52
Nathan Kot <me@nathankot.com>
2015-07-29 11:24:50 +0900

    Allow overriding init:frame:

--- Hermes/HermesNotificationView.swift
+++ Hermes/HermesNotificationView.swift
@@ -7,8 +7,8 @@ public class HermesNotificationView: UIView {
   required public init(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-    
-  override init(frame: CGRect) {
+
+  public override init(frame: CGRect) {
     super.init(frame: frame)
     addSubview(contentView)
   }
