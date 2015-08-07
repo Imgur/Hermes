5988f28e8d88bcd61f76b5cbdf57968f7990923e
Matt Lebl <matt@tinkerlab.io>
2015-08-07 23:16:34 +0200

    Added some helpful Quick Help comments

--- Hermes/HermesNotification.swift
+++ Hermes/HermesNotification.swift
@@ -6,7 +6,8 @@ protocol HermesNotificationDelegate: class {
 
 public class HermesNotification: NSObject {
   weak var delegate: HermesNotificationDelegate?
-    
+  
+  /// The text of the notification
   public var text: String? {
     set(text) {
       if (text != nil) {
@@ -19,13 +20,27 @@ public class HermesNotification: NSObject {
       return attributedText?.string
     }
   }
+  
+  /// Text, but with fancy attributes instead of a regular `String`
   public var attributedText: NSAttributedString?
+  
+  /// The color of the bottom bar on the notification
   public var color: UIColor?
+  
+  /// The image to be displayed along with the notification
   public var image: UIImage?
+  
+  /// The URL to the image
   public var imageURL: NSURL?
   public var tag: String?
+  
+  /// The path to the sound to be played along with the notification
   public var soundPath: String?
+  
+  /// The code that should be executed when the notification is tapped on
   public var action: ((HermesNotification) -> Void)? = nil
+  
+  /// Should the notification close automatically?
   public var autoClose = true {
     didSet {
       if oldValue != autoClose {
@@ -33,7 +48,11 @@ public class HermesNotification: NSObject {
       }
     }
   }
+  
+  /// The time interval that the notification should stay on screen before self-dismissing
   public var autoCloseTimeInterval: NSTimeInterval = 3
+  
+  /// Force the notification to perform it's action
   public func invokeAction() {
     if action != nil {
       action!(self)
